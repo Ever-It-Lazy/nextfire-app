b76e60b870e6ec9fc5ff0df84f9e377dda7a361e
Everett Lindsay <everettsjunk@gmail.com>
2023-01-30 18:22:28 -0500

    Google SignIn

--- pages/enter.js
+++ pages/enter.js
@@ -1,7 +1,40 @@
-export default function EnterPage({ }) {
+import { auth, googleAuthProvider } from '@/lib/firebase';
+import { signInWithPopup } from 'firebase/auth';
+
+export default function Enter({ }) {
+	const user = null;
+	const username = null;
+
 	return (
 		<main>
-			<h1>Sign Up</h1>
+			{user ?
+				!username ? <UsernameForm /> : <SignOutButton />
+				: <SignInButton />
+			}
 		</main>
-	)
+	);
+}
+
+function SignInButton() {
+	const signInWithGoogle = async () => {
+		await signInWithPopup(auth, googleAuthProvider);
+	}
+
+	return (
+		<button className='btn-google' onClick={signInWithGoogle}>
+			<img src={'/google.png'} /> Sign in with Google
+		</button>
+	);
+}
+
+function SignOutButton() {
+	return (
+		<button onClick={() => auth.signOut()}>
+			Sign Out
+		</button>
+	);
+}
+
+function UsernameForm() {
+	return
 }
