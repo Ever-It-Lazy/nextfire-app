334a8f28e2c2811b275c74332361403da18be127
Everett Lindsay <everettsjunk@gmail.com>
2023-02-01 16:03:54 -0500

    Admin Pages

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,10 +1,13 @@
+import AuthCheck from "@/components/AuthCheck";
 import Metatags from "@/components/Metatags";
 
-export default function AdminPostsPage({ }) {
+export default function AdminPostsPage(props) {
 	return (
 		<main>
-			<Metatags title="admin page" />
-			<h1>Admin posts</h1>
+			<AuthCheck>
+				<Metatags title="admin page" />
+				<h1>Admin posts</h1>
+			</AuthCheck>
 		</main>
 	)
 }
