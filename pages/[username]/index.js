ab087ce6e928c51cd843b544bd2c5b83b91b3f3e
Everett Lindsay <everettsjunk@gmail.com>
2023-01-31 21:11:07 -0500

    User Profile Page

--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -1,7 +1,40 @@
-export default function AdminRoute({ }) {
+import UserProfile from "@/components/UserProfile";
+import PostFeed from "@/components/PostFeed";
+import { getUserWithUsername, postToJSON } from "@/lib/firebase";
+import { collection, where, query, orderBy, getDocs, limit } from "firebase/firestore";
+
+
+export async function getServerSideProps(context) {
+	const { username } = context.query;
+
+	const userDoc = await getUserWithUsername(username);
+
+	let user = null;
+	let posts = null;
+
+	if (userDoc) {
+		user = userDoc.data();
+		const postsQuery = query(
+			collection(userDoc.ref, 'posts'),
+			where('published', '==', true),
+			orderBy('createdAt', 'desc'),
+			limit(5)
+		);
+		posts = await getDocs(postsQuery)
+// TODO: no posts yet
+		posts =  [].map(postToJSON);
+	}
+
+	return {
+		props: { user, posts },
+	};
+}
+
+export default function UserProfilePage({ user, posts }) {
 	return (
 		<main>
-			<h1>blah</h1>
+			<UserProfile user={user} />
+			<PostFeed posts={posts} />
 		</main>
 	)
 }
