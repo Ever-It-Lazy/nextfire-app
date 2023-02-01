d0931a92344271d742fefaec8d19e7b34ba0c371
Everett Lindsay <everettsjunk@gmail.com>
2023-02-01 13:37:43 -0500

    Custom 404; got the Feed working

--- pages/[username]/[slug].js
+++ pages/[username]/[slug].js
@@ -1,7 +1,63 @@
-export default function PostPage({ }) {
-	return (
-		<main>
+import { firestore, getUserWithUsername, postToJSON } from "@/lib/firebase";
+import { collection, collectionGroup, doc, getDoc, getDocs, query, setDoc } from "firebase/firestore";
+import { useDocumentData } from "react-firebase-hooks/firestore";
+import styles from '@/styles/Post.module.css';
+import PostContent from '@/components/PostContent';
 
+export async function getStaticProps({ params }) {
+	const { username, slug } = params;
+	const userDoc = await getUserWithUsername(username);
+
+	let post;
+	let path;
+
+	if (userDoc) {
+		const postRef = doc(collection(userDoc.ref, 'posts'), slug);
+		post = postToJSON(await getDoc(postRef));
+
+		path = postRef.path;
+
+		return {
+			props: { post, path },
+			revalidate: 5000
+		};
+	}
+}
+
+export async function getStaticPaths() {
+
+	const snapshot = await getDocs(collectionGroup(firestore, 'posts'));
+
+	const paths = snapshot.docs.map((doc) => {
+		const { slug, username } = doc.data();
+		return {
+			params: { username, slug }
+		};
+	});
+
+	return {
+		paths,
+		fallback: 'blocking'
+	};
+}
+
+export default function Post(props) {
+	const postRef = getDoc(firestore, props.path);
+	const [realtimePost] = useDocumentData(postRef);
+
+	const post = realtimePost || props.post;
+
+	return (
+		<main className={styles.container}>
+			<section>
+				<PostContent post={post} />
+			</section>
+
+			<aside className="card">
+				<p>
+					<strong>{post.hearCount || 0} 🤍</strong>
+				</p>
+			</aside>
 		</main>
 	)
 }
